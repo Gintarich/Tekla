e810095627d410b9652b66484af9b03a180eb7bf
Gintars Briedis <gintars.briedis@gmail.com>
2021-04-08 11:10:51 +0300

    Added some code

--- TeklaConsole/Program.cs
+++ TeklaConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tekla.Structures.Model;
 
 namespace TeklaConsole
 {
@@ -10,6 +11,8 @@ namespace TeklaConsole
     {
         static void Main(string[] args)
         {
+            Model model = new Model();
+
         }
     }
 }
