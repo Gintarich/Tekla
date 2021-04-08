de262966d657fe3617668224be692dbea7be64b5
Gintars Briedis <gintars.briedis@gmail.com>
2021-04-08 14:33:46 +0300

    Added some code

---
+++ HelperMethods/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+
+namespace HelperMethods
+{
+    public static class ExtensionMethods
+    {
+        public static List<ModelObject> ToList(this ModelObjectEnumerator enumerator)
+        {
+            var modelObjects = new List<ModelObject>();
+            while (enumerator.MoveNext())
+            {
+                var modelObject = enumerator.Current;
+                modelObjects.Add(modelObject);
+            }
+            return modelObjects;
+        }
+	}
+
+    
+}
--- TeklaConsole/Program.cs
+++ TeklaConsole/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelperMethods;
+using Tekla.Structures;
 using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
 
 namespace TeklaConsole
 {
@@ -11,8 +14,24 @@ namespace TeklaConsole
     {
         static void Main(string[] args)
         {
+            List<Identifier> idsList = new List<Identifier>();
             Model model = new Model();
+            if (model.GetConnectionStatus())
+            {
+                Console.WriteLine("Viss izdevas");
+                Picker picker = new Picker();
+                Part selectedObj = (Part)picker.PickObject(Picker.PickObjectEnum.PICK_ONE_PART);
+                var atverumi = selectedObj.GetBooleans();
+                while (atverumi.MoveNext())
+                {
+                    var id =atverumi.Current.Identifier;
+                    idsList.Add(id);
 
+                    Console.WriteLine(id.ToString());
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
