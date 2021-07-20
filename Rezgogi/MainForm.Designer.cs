1693f76afbadc730bc8885e77c94edcfa6443cf4
Gintars Briedis <gintars.briedis@gmail.com>
2021-07-20 15:56:00 +0300

    dasda

---
+++ Rezgogi/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Rezgogi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_Garums = new System.Windows.Forms.TextBox();
+            this.structuresExtender1 = new Tekla.Structures.Dialog.StructuresExtender();
+            this.l_Garums = new System.Windows.Forms.Label();
+            this.l_Platums = new System.Windows.Forms.Label();
+            this.b_Platums = new System.Windows.Forms.TextBox();
+            this.okApplyModifyGetOnOffCancel1 = new Tekla.Structures.Dialog.UIControls.OkApplyModifyGetOnOffCancel();
+            this.saveLoad1 = new Tekla.Structures.Dialog.UIControls.SaveLoad();
+            this.SuspendLayout();
+            // 
+            // b_Garums
+            // 
+            this.structuresExtender.SetAttributeName(this.b_Garums, null);
+            this.structuresExtender1.SetAttributeName(this.b_Garums, "Garums");
+            this.structuresExtender.SetAttributeTypeName(this.b_Garums, null);
+            this.structuresExtender1.SetAttributeTypeName(this.b_Garums, "double");
+            this.structuresExtender.SetBindPropertyName(this.b_Garums, null);
+            this.structuresExtender1.SetBindPropertyName(this.b_Garums, null);
+            this.b_Garums.Location = new System.Drawing.Point(99, 71);
+            this.b_Garums.Name = "b_Garums";
+            this.b_Garums.Size = new System.Drawing.Size(100, 20);
+            this.b_Garums.TabIndex = 0;
+            // 
+            // l_Garums
+            // 
+            this.structuresExtender.SetAttributeName(this.l_Garums, null);
+            this.structuresExtender1.SetAttributeName(this.l_Garums, null);
+            this.structuresExtender.SetAttributeTypeName(this.l_Garums, null);
+            this.structuresExtender1.SetAttributeTypeName(this.l_Garums, null);
+            this.l_Garums.AutoSize = true;
+            this.structuresExtender1.SetBindPropertyName(this.l_Garums, null);
+            this.structuresExtender.SetBindPropertyName(this.l_Garums, null);
+            this.l_Garums.Location = new System.Drawing.Point(50, 74);
+            this.l_Garums.Name = "l_Garums";
+            this.l_Garums.Size = new System.Drawing.Size(43, 13);
+            this.l_Garums.TabIndex = 3;
+            this.l_Garums.Text = "Garums";
+            // 
+            // l_Platums
+            // 
+            this.structuresExtender.SetAttributeName(this.l_Platums, null);
+            this.structuresExtender1.SetAttributeName(this.l_Platums, null);
+            this.structuresExtender.SetAttributeTypeName(this.l_Platums, null);
+            this.structuresExtender1.SetAttributeTypeName(this.l_Platums, null);
+            this.l_Platums.AutoSize = true;
+            this.structuresExtender1.SetBindPropertyName(this.l_Platums, null);
+            this.structuresExtender.SetBindPropertyName(this.l_Platums, null);
+            this.l_Platums.Location = new System.Drawing.Point(50, 100);
+            this.l_Platums.Name = "l_Platums";
+            this.l_Platums.Size = new System.Drawing.Size(44, 13);
+            this.l_Platums.TabIndex = 5;
+            this.l_Platums.Text = "Platums";
+            // 
+            // b_Platums
+            // 
+            this.structuresExtender.SetAttributeName(this.b_Platums, null);
+            this.structuresExtender1.SetAttributeName(this.b_Platums, "Platums");
+            this.structuresExtender.SetAttributeTypeName(this.b_Platums, null);
+            this.structuresExtender1.SetAttributeTypeName(this.b_Platums, "double");
+            this.structuresExtender.SetBindPropertyName(this.b_Platums, null);
+            this.structuresExtender1.SetBindPropertyName(this.b_Platums, null);
+            this.b_Platums.Location = new System.Drawing.Point(99, 97);
+            this.b_Platums.Name = "b_Platums";
+            this.b_Platums.Size = new System.Drawing.Size(100, 20);
+            this.b_Platums.TabIndex = 4;
+            // 
+            // okApplyModifyGetOnOffCancel1
+            // 
+            this.structuresExtender.SetAttributeName(this.okApplyModifyGetOnOffCancel1, null);
+            this.structuresExtender1.SetAttributeName(this.okApplyModifyGetOnOffCancel1, null);
+            this.structuresExtender.SetAttributeTypeName(this.okApplyModifyGetOnOffCancel1, null);
+            this.structuresExtender1.SetAttributeTypeName(this.okApplyModifyGetOnOffCancel1, null);
+            this.structuresExtender.SetBindPropertyName(this.okApplyModifyGetOnOffCancel1, null);
+            this.structuresExtender1.SetBindPropertyName(this.okApplyModifyGetOnOffCancel1, null);
+            this.okApplyModifyGetOnOffCancel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.okApplyModifyGetOnOffCancel1.Location = new System.Drawing.Point(0, 421);
+            this.okApplyModifyGetOnOffCancel1.Name = "okApplyModifyGetOnOffCancel1";
+            this.okApplyModifyGetOnOffCancel1.Size = new System.Drawing.Size(800, 29);
+            this.okApplyModifyGetOnOffCancel1.TabIndex = 6;
+            // 
+            // saveLoad1
+            // 
+            this.structuresExtender.SetAttributeName(this.saveLoad1, null);
+            this.structuresExtender1.SetAttributeName(this.saveLoad1, null);
+            this.structuresExtender1.SetAttributeTypeName(this.saveLoad1, null);
+            this.structuresExtender.SetAttributeTypeName(this.saveLoad1, null);
+            this.saveLoad1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.structuresExtender1.SetBindPropertyName(this.saveLoad1, null);
+            this.structuresExtender.SetBindPropertyName(this.saveLoad1, null);
+            this.saveLoad1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.saveLoad1.HelpFileType = Tekla.Structures.Dialog.UIControls.SaveLoad.HelpFileTypeEnum.General;
+            this.saveLoad1.HelpKeyword = "";
+            this.saveLoad1.HelpUrl = "";
+            this.saveLoad1.Location = new System.Drawing.Point(0, 0);
+            this.saveLoad1.Name = "saveLoad1";
+            this.saveLoad1.SaveAsText = "";
+            this.saveLoad1.Size = new System.Drawing.Size(800, 43);
+            this.saveLoad1.TabIndex = 7;
+            this.saveLoad1.UserDefinedHelpFilePath = null;
+            // 
+            // MainForm
+            // 
+            this.structuresExtender.SetAttributeName(this, null);
+            this.structuresExtender1.SetAttributeName(this, null);
+            this.structuresExtender.SetAttributeTypeName(this, null);
+            this.structuresExtender1.SetAttributeTypeName(this, null);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.structuresExtender.SetBindPropertyName(this, null);
+            this.structuresExtender1.SetBindPropertyName(this, null);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveLoad1);
+            this.Controls.Add(this.okApplyModifyGetOnOffCancel1);
+            this.Controls.Add(this.l_Platums);
+            this.Controls.Add(this.b_Platums);
+            this.Controls.Add(this.l_Garums);
+            this.Controls.Add(this.b_Garums);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox b_Garums;
+        private Tekla.Structures.Dialog.StructuresExtender structuresExtender1;
+        private System.Windows.Forms.Label l_Garums;
+        private System.Windows.Forms.Label l_Platums;
+        private System.Windows.Forms.TextBox b_Platums;
+        private Tekla.Structures.Dialog.UIControls.OkApplyModifyGetOnOffCancel okApplyModifyGetOnOffCancel1;
+        private Tekla.Structures.Dialog.UIControls.SaveLoad saveLoad1;
+    }
+}
